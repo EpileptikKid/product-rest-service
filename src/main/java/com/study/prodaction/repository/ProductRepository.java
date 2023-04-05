c207bc1ffdea114937b798f14de4dc49897e815b
Voloskovets Andrii <k1d.ep1lept1k@gmail.com>
2023-04-05 04:28:32 +0300

    Перезапуск проекту, з використанням Spring boot. Ініціалізуючий коміт

--- src/main/java/com/study/prodaction/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.study.prodaction.repository;
-
-import com.study.prodaction.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-}
